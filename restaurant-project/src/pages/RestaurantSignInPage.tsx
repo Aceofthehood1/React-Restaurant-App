e4eb04d5bdcb494e2c1a8d0df30b7ecfbe4b5e67
Geoffrey Ameble <geoffreyameble81@gmail.com>
2023-07-28 20:42:38 +0100

    Created pages and components for the restaurant representative sign in and sign up

---
+++ restaurant-project/src/pages/RestaurantSignInPage.tsx
@@ -0,0 +1,11 @@
+import RestaurantSignInForm from "../components/RestaurantSignInForm";
+function RestaurantSignInPage() {
+    return (
+      <>
+        <h1 id="head">Restaurant Sign In Page</h1>
+        <RestaurantSignInForm></RestaurantSignInForm>
+      </>
+    );
+  }
+  
+  export default RestaurantSignInPage;
