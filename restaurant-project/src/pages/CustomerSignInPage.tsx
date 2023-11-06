d9b3806bd6ea52815bae192c80c0895dfbecb49e
Geoffrey Ameble <geoffreyameble81@gmail.com>
2023-11-06 18:58:18 +0000

    Added tailwind css and made media queries for all pages

--- restaurant-project/src/pages/CustomerSignInPage.tsx
+++ restaurant-project/src/pages/CustomerSignInPage.tsx
@@ -1,9 +1,25 @@
-import CustomerSignInForm from "../components/CustomerSignInForm";
+import { Link } from "react-router-dom";
 function CustomerSignInPage() {
   return (
     <>
-    <h1 id="head">Customer Sign In Page</h1>
-    <CustomerSignInForm></CustomerSignInForm>
+    <h1 className="text-4xl m-2" id="head">Customer Sign In Page</h1>
+    <form className="form mb-10">
+        <p id="form-head">Sign In</p>
+        <label htmlFor="email-address">
+          Email
+          <input id="email-address" type="email"></input>
+        </label>
+        <label htmlFor="password">
+          Password
+          <input id="password" type="password"></input>
+        </label>
+        <button className="btn btn-three">
+          <span>Sign Up</span>
+        </button>
+        <Link id="form-link" to="/customerSignUp">
+          Dont have an account? Click here to sign up
+        </Link>
+      </form>
     </>
   );
 }
