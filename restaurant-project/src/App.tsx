272086e5b1d6cfca9513e9aca2c1b1f18f25927f
Geoffrey Ameble <geoffreyameble81@gmail.com>
2023-11-08 13:20:28 +0000

    Made changes to styles

--- restaurant-project/src/App.tsx
+++ restaurant-project/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className= "text-4xl m-2" id="head">Home Page</h1>
-      <div className="justify-center align-center" id="home-box">
+      <div className="flex flex-col justify-center items-center w-10/12" id="home-box">
         <Link to="/customerSignUp" className="link-box">
           <img className="home-image" src={customerImg}></img>
           <p className="text-center text-3xl">Sign in as a customer</p>
