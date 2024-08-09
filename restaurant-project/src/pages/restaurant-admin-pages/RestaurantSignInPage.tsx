a6b151ae5db61bc2f0d150a9ef0fd3d2da5c4281
Aceofthehood1 <geoffreyameble@gmail.com>
2024-08-09 13:44:54 +0100

    Added login functionality for both customer and representative pages so when correct details are entered, user is redirected to appropriate page. Users will also get alerts if they enter wrong values

--- restaurant-project/src/pages/restaurant-admin-pages/RestaurantSignInPage.tsx
+++ restaurant-project/src/pages/restaurant-admin-pages/RestaurantSignInPage.tsx
@@ -1,7 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { MongoClient } from "mongodb";
+import axios from "axios";
+import { useState, useEffect } from "react";
 function RestaurantSignInPage() {
+  const [email, setEmail] = useState<String>();
+  const [password, setPassword] = useState<String>();
 
+  const navigate = useNavigate();
+
+  const Check = (e: { preventDefault: () => any }) => {
+    e.preventDefault();
+    if (email && password) {
+      axios
+        .post("http://localhost:3001/loginRepresentative", { email, password })
+        .then((result) => {
+          console.log(result);
+          if (result.data == "Logged in Successfully") {
+            //from backend the result value is taken and checked to see if user was able to login successfully if it was possible they are redirected to a different page
+            navigate("/restaurantAdminPage");
+          }else if(result.data == "The password is incorrect"){
+            alert("The password is incorrect")
+          } else {
+            alert("Email does not exist");
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      e.preventDefault();
+      alert("Do not leave any fields empty");
+    }
+  };
   return (
     <>
       <h1 className="text-5xl m-2" id="head">
@@ -16,17 +44,25 @@ function RestaurantSignInPage() {
           <p id="form-head">Sign In</p>
           <label htmlFor="email-address">
             Email
-            <input id="email-address" type="email" placeholder="Email"></input>
+            <input
+              id="email-address"
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
           </label>
           <label htmlFor="password">
             Password
-            <input id="password" type="password" placeholder="Password"></input>
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
           </label>
-          <Link to="/restaurantAdminPage">
-            <button className="btn btn-three">
-              <span>Sign Up</span>
-            </button>
-          </Link>
+          <button className="btn btn-three" onClick={Check}>
+            <span>Sign Up</span>
+          </button>
           <Link id="form-link" to="/restaurantSignUp">
             Dont have an account? Click here to sign up
           </Link>
